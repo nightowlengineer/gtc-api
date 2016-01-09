a614941095c774a338e51933d48cc75b895dfc18
James Milligan <james@milligan.tv>
2016-01-09 00:06:51 +0000

    Added UtilityHelper with isNullOrEmpty method

---
+++ api/src/main/java/uk/org/gtc/api/UtilityHelper.java
@@ -0,0 +1,15 @@
+package uk.org.gtc.api;
+
+public class UtilityHelper
+{
+	
+	public UtilityHelper()
+	{
+	
+	}
+	
+	public static Boolean isNullOrEmpty(final String string)
+	{
+		return string == null || string.isEmpty();
+	}
+}
