6353fd68b67839c349b470fd9fe6b1c2697ba9dc
James Milligan <james@milligan.tv>
2016-01-09 00:46:01 +0000

    Add canUseLogo method to MemberType

--- api/src/main/java/uk/org/gtc/api/domain/MemberType.java
+++ api/src/main/java/uk/org/gtc/api/domain/MemberType.java
@@ -2,5 +2,25 @@ package uk.org.gtc.api.domain;
 
 public enum MemberType
 {
-	FULL, ASSOCIATE, STUDENT, ACADEMIC, SPONSOR, AFFILIATE, RETIRED, FELLOW, HONORARY
+	FULL, ASSOCIATE, STUDENT, ACADEMIC, SPONSOR, AFFILIATE, RETIRED, FELLOW, HONORARY;
+	
+	public Boolean canUseLogo()
+	{
+		switch (this)
+		{
+		case FULL:
+		case SPONSOR:
+		case FELLOW:
+		case HONORARY:
+			return true;
+		case ASSOCIATE:
+		case STUDENT:
+		case ACADEMIC:
+		case AFFILIATE:
+		case RETIRED:
+			return false;
+		default:
+			throw new RuntimeException("No value was set for this MemberType");
+		}
+	}
 }
