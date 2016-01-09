12e3fd864de7c01e4ff90ff86930cd748b3e983a
James Milligan <james@milligan.tv>
2016-01-09 00:06:22 +0000

    Added method to MemberStatus to determine whether membershipNumber is required

--- api/src/main/java/uk/org/gtc/api/domain/MemberStatus.java
+++ api/src/main/java/uk/org/gtc/api/domain/MemberStatus.java
@@ -2,5 +2,24 @@ package uk.org.gtc.api.domain;
 
 public enum MemberStatus
 {
-	APPLIED, DECLINED, APPROVED, INVOICED, PAID, CURRENT, LAPSED, REMOVED
+	APPLIED, DECLINED, APPROVED, INVOICED, PAID, CURRENT, LAPSED, REMOVED;
+	
+	public Boolean requiresMemberNumber()
+	{
+		switch (this)
+		{
+		case APPLIED:
+		case DECLINED:
+		case APPROVED:
+		case INVOICED:
+		case PAID:
+			return false;
+		case CURRENT:
+		case LAPSED:
+		case REMOVED:
+			return true;
+		default:
+			throw new RuntimeException("No requirement was set for this MemberType");
+		}
+	}
 }
