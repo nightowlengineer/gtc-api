d1450d961f561d0c54a90505b1faa6c7293f02f9
James Milligan <james@milligan.tv>
2017-04-30 19:35:35 +0100

    TheGTC/gtc-office#8 Map any case of Salutation

--- api/src/main/java/uk/org/gtc/api/domain/Salutation.java
+++ api/src/main/java/uk/org/gtc/api/domain/Salutation.java
@@ -1,6 +1,21 @@
 package uk.org.gtc.api.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Salutation
 {
-	MR, MRS, MISS, MS, MX, DR, SIR
+	MR, MRS, MISS, MS, MX, DR, SIR;
+	
+	@JsonCreator
+	public static Salutation fromString(final String key)
+	{
+		for (final Salutation sal : Salutation.values())
+		{
+			if (sal.name().equalsIgnoreCase(key))
+			{
+				return sal;
+			}
+		}
+		return null;
+	}
 }
